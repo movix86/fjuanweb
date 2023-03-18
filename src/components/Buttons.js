32280a8267634ea35f32dd787df56082f4b9ab47
movix86 <movix86@gmail.com>
2023-03-17 19:59:57 -0500

    feat: Add info the another buttons, the changes is in the git commit

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -8,47 +8,133 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 import Badge from 'react-bootstrap/Badge';
 
+/* Imagens */
+import skills from '../img/image-skills.png';
+import mvc from '../img/image-mvc.png';
+import cssI from '../img/image-css.png'
+import pythonI from '../img/image-python.png'
+
 import { useState, useEffect } from 'react';
+
+
 
 const Buttons = () => {
 
   const  [boxState, usetBoxState] = useState("");
+  const uri = window.location.href + "img/";
 
   if(boxState=="skills"){
-    return (
-      <>
-          <Row>
-            <Col sm={12}>
-              <ButtonGroup aria-label="Basic example">
-              <Button variant="secondary" title="Habilidades" onClick={()=>{usetBoxState("skills")}}>Skills</Button>
-              <Button variant="secondary" onClick={()=>{alert("I know Styles Model MVC")}}>MVC</Button>
-              <Button variant="secondary" onClick={()=>{alert("I know Styles CSS")}}>Styles</Button>
-              <Button variant="secondary" onClick={()=>{alert("I am learning Python in this 2023")}}>Python</Button>
-              </ButtonGroup>
-            </Col>
-          </Row>
-          <Row>
-            <Col sm={6} className='skills'>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-              </p>
-            </Col>
-            <Col sm={6} className='image-skills'>
-              <img src='https://cdn.pixabay.com/photo/2019/04/04/08/01/icon-4102192_1280.png' />
-            </Col>
-          </Row>
-      </>
-    );
-  }else{
     return (
       <>
         <Row>
           <Col sm={12}>
             <ButtonGroup aria-label="Basic example">
               <Button variant="secondary" title="Habilidades" onClick={()=>{usetBoxState("skills")}}>Skills</Button>
-              <Button variant="secondary" onClick={()=>{alert("I know Styles Model MVC")}}>MVC</Button>
-              <Button variant="secondary" onClick={()=>{alert("I know Styles CSS")}}>Styles</Button>
-              <Button variant="secondary" onClick={()=>{alert("I am learning Python in this 2023")}}>Python</Button>
+              <Button variant="secondary" onClick={()=>{usetBoxState("mvc")}}>MVC</Button>
+              <Button variant="secondary" onClick={()=>{usetBoxState("styles")}}>Styles</Button>
+              <Button variant="secondary" onClick={()=>{usetBoxState("python")}}>Python</Button>
+            </ButtonGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={6} className='skills'>
+            <ul>
+              <p>PHP (4 años)</p>
+              <p>Laravel (2 años)</p>
+              <p>Mysql (3 años)</p>
+              <p>Github (3 años)</p>
+            </ul>
+          </Col>
+          <Col sm={6} className='image-skills'>
+            <img src={skills} />
+          </Col>
+        </Row>
+      </>
+    );
+  }else if(boxState=="mvc"){
+    return (
+      <>
+        <Row>
+          <Col sm={12}>
+            <ButtonGroup aria-label="Basic example">
+              <Button variant="secondary" title="Habilidades" onClick={()=>{usetBoxState("skills")}}>Skills</Button>
+              <Button variant="secondary" onClick={()=>{usetBoxState("mvc")}}>MVC</Button>
+              <Button variant="secondary" onClick={()=>{usetBoxState("styles")}}>Styles</Button>
+              <Button variant="secondary" onClick={()=>{usetBoxState("python")}}>Python</Button>
+            </ButtonGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={6} className='skills'>
+            <ul>  
+              <p>MVC web development with php</p>
+              <p>MVC web development with Laravel</p>
+              <p>Entity-relationship model with mysql</p>
+            </ul>
+          </Col>
+          <Col sm={6} className='image-skills'>
+            <img src={mvc} />
+          </Col>
+        </Row>
+      </>
+    );
+  }else if(boxState=="styles"){
+    return (
+      <>
+        <Row>
+          <Col sm={12}>
+            <ButtonGroup aria-label="Basic example">
+              <Button variant="secondary" title="Habilidades" onClick={()=>{usetBoxState("skills")}}>Skills</Button>
+              <Button variant="secondary" onClick={()=>{usetBoxState("mvc")}}>MVC</Button>
+              <Button variant="secondary" onClick={()=>{usetBoxState("styles")}}>Styles</Button>
+              <Button variant="secondary" onClick={()=>{usetBoxState("python")}}>Python</Button>
+            </ButtonGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={6} className='skills'>
+            <p>Configuration of CSS styles for web sites and landings</p>
+          </Col>
+          <Col sm={6} className='image-skills'>
+            <img src={cssI} />
+          </Col>
+        </Row>
+      </>
+    );
+  }else if(boxState=="python"){
+    return (
+      <>
+        <Row>
+          <Col sm={12}>
+            <ButtonGroup aria-label="Basic example">
+              <Button variant="secondary" title="Habilidades" onClick={()=>{usetBoxState("skills")}}>Skills</Button>
+              <Button variant="secondary" onClick={()=>{usetBoxState("mvc")}}>MVC</Button>
+              <Button variant="secondary" onClick={()=>{usetBoxState("styles")}}>Styles</Button>
+              <Button variant="secondary" onClick={()=>{usetBoxState("python")}}>Python</Button>
+            </ButtonGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={6} className='skills'>
+              <p>Web development with Python, one of the best backend languages available today</p>
+          </Col>
+          <Col sm={6} className='image-skills'>
+            <img src={pythonI} />
+          </Col>
+        </Row>
+      </>
+    );
+  }
+  else{
+    return (
+      <>
+        <Row>
+          <Col sm={12}>
+            <ButtonGroup aria-label="Basic example">
+              <Button variant="secondary" title="Habilidades" onClick={()=>{usetBoxState("skills")}}>Skills</Button>
+              <Button variant="secondary" onClick={()=>{usetBoxState("mvc")}}>MVC</Button>
+              <Button variant="secondary" onClick={()=>{usetBoxState("styles")}}>Styles</Button>
+              <Button variant="secondary" onClick={()=>{usetBoxState("python")}}>Python</Button>
             </ButtonGroup>
           </Col>
         </Row>
